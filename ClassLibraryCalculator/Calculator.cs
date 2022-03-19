e35da35a953df599ed3db452766c134609677503
Dyaha1403 <an636646@gmail.com>
2022-03-19 18:49:25 +0200

    Add files

---
+++ ClassLibraryCalculator/Calculator.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCalculator
+{
+    public class Calculator
+    {
+        public string num_1 { get; set; } = "";
+        public string num_2 { get; set; } = "";
+        private double result = 0;
+        public string operation { get; set; } = "";
+        public double firstLimit { get; set; } = -10000;
+        public double secondLimit { get; set; } = 800000;
+
+        public void Clean()
+        {
+            num_1 = "";
+            num_2 = "";
+            result = 0;
+            operation = "";
+        }
+
+        public string CalculationResult()
+        {
+            if(operation == "")
+            {
+                return "";
+            }
+
+            switch (operation)
+            {
+                case "+":
+                    result = Convert.ToDouble(num_1) + Convert.ToDouble(num_2);
+                    break;
+                case "-":
+                    result = Convert.ToDouble(num_1) - Convert.ToDouble(num_2);
+                    break;
+                case "/":
+                    result = Convert.ToDouble(num_1) / Convert.ToDouble(num_2);
+                    break;   
+                case "*":
+                    result = Convert.ToDouble(num_1) * Convert.ToDouble(num_2);
+                    break;
+            }
+
+            return Convert.ToString(Math.Round(result, 3));
+        }
+        public double CalculatorSin(double num)
+        {
+            return Math.Round(Math.Sin(num * Math.PI / 180), 3);
+        }
+        public double CalculatorRoot(double num)
+        {
+            return Math.Round(Math.Sqrt(num), 3);
+        }
+        public double CalculatorSquare(double num)
+        {
+            return Math.Round(Math.Pow(num, 2), 3);
+        }
+        public bool InputNum(string text, string num)
+        {
+            if (!CheckCountDecimalNum(text) || !CheckRange(text + num))
+            {
+                return false;
+            }
+
+            return true;
+        }
+        private bool CheckCountDecimalNum(string text)
+        {
+            if (text.IndexOf(",") != -1)
+            {
+                int index = text.IndexOf(",");
+
+                if (text.Length - (index + 1) >= 3)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public bool CheckRange(string text)
+        {
+            if(Convert.ToDouble(text) < firstLimit 
+            || Convert.ToDouble(text) > secondLimit)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ DyahaCalculator/MainWindow.xaml.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using ClassLibraryCalculator;
+
+namespace DyahaCalculator
+{
+    public partial class MainWindow : Window
+    {
+
+        Calculator Calculator = new Calculator();
+        bool ButtonRangeOnClick = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InputBox.Text = "0";
+            TextBoxFirstLimit.Text = Calculator.firstLimit.ToString();
+            TextBoxSecondLimit.Text = Calculator.secondLimit.ToString();
+        }
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void ButtonClean_Click(object sender, RoutedEventArgs e)
+        {
+            if (InputBox.Text == "На ноль делить нельзя")
+            {
+                InputBox.FontSize = 30;
+                ButtonOperationGrid.IsEnabled = true;
+                ButtonChangeSign.IsEnabled = true;
+            }
+
+            InputBox.Text = "0";
+            ResultText.Content = "";
+            Calculator.Clean();
+        }
+        private void ButtonErase_Click(object sender, RoutedEventArgs e)
+        {
+            if (InputBox.Text == "На ноль делить нельзя")
+            {
+                InputBox.FontSize = 30;
+                InputBox.Text = "0";
+                ButtonOperationGrid.IsEnabled = true;
+                ButtonChangeSign.IsEnabled = true;
+            }
+
+            string ChangedText = InputBox.Text.Remove(InputBox.Text.Length - 1);
+
+            if(Convert.ToString(ResultText.Content).IndexOf("=") != -1)
+            {
+                ResultText.Content = "";
+            }
+
+            if (ChangedText == "")
+            {
+                ChangedText = "0";
+            }
+            
+            if(ChangedText[ChangedText.Length - 1] == ',')
+            {
+                ChangedText = ChangedText.Remove(ChangedText.Length - 1);
+            }
+
+            InputBox.Text = ChangedText;
+        }
+        private void ButtonNum_Click(object sender, RoutedEventArgs e)
+        {
+            Button Num = (Button)sender;
+
+            if (InputBox.Text == "На ноль делить нельзя")
+            {
+                InputBox.FontSize = 30;
+                InputBox.Text = "0";
+                ButtonOperationGrid.IsEnabled = true;
+                ButtonChangeSign.IsEnabled = true;
+            }
+            
+            if (!Calculator.InputNum(InputBox.Text, Num.Content.ToString()))
+            {
+                return;
+            }
+            
+            if (InputBox.Text == "0")
+            {
+                InputBox.Text = "";
+            }
+            
+            if (Convert.ToString(ResultText.Content).IndexOf("=") != -1)
+            {
+                ResultText.Content = "";
+            }
+
+            InputBox.Text += Num.Content.ToString();
+        }
+        private void ButtonDefaultOperation_Click(object sender, RoutedEventArgs e)
+        {
+            Button Operation = (Button)sender;
+
+            if (Calculator.operation == "")
+            {
+                Calculator.num_1 = InputBox.Text;
+                InputOperation(Operation.Content.ToString());
+                return;
+            }
+
+            if (ResultText.Content.ToString().IndexOf("=") == -1)
+            {
+                Calculator.num_2 = InputBox.Text;
+                Calculator.num_1 = Calculator.CalculationResult();
+            }
+            else
+            {
+                Calculator.num_1 = InputBox.Text;
+                Calculator.num_2 = "";
+            }
+
+            InputOperation(Operation.Content.ToString());
+        }
+        private void ButtonSin_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToString(ResultText.Content).IndexOf("=") != -1)
+            {
+                ResultText.Content = "";
+                Calculator.num_2 = "";
+            }
+
+            InputBox.Text = Calculator.CalculatorSin(Convert.ToDouble(InputBox.Text)).ToString();
+        }
+        private void ButtonRoot_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToString(ResultText.Content).IndexOf("=") != -1)
+            {
+                ResultText.Content = "";
+                Calculator.num_2 = "";
+            }
+
+            InputBox.Text = Calculator.CalculatorRoot(Convert.ToDouble(InputBox.Text)).ToString();
+        }
+        private void ButtonSquare_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToString(ResultText.Content).IndexOf("=") != -1)
+            {
+                ResultText.Content = "";
+                Calculator.num_2 = "";
+            }
+
+            InputBox.Text = Calculator.CalculatorSquare(Convert.ToDouble(InputBox.Text)).ToString();
+        }
+        private void ButtonChangeSign_Click(object sender, RoutedEventArgs e)
+        {
+            if(InputBox.Text == "0" || !Calculator.CheckRange((Convert.ToDouble(InputBox.Text) * -1).ToString()))
+            {
+                return;
+            }
+
+            if (Convert.ToString(ResultText.Content).IndexOf("=") != -1)
+            {
+                ResultText.Content = "";
+                Calculator.num_2 = "";
+            }
+
+            InputBox.Text = (Convert.ToDouble(InputBox.Text) * -1).ToString();
+            Calculator.num_1 = InputBox.Text;
+        }
+        private void ButtonResult_Click(object sender, RoutedEventArgs e)
+        {
+            if (InputBox.Text == "На ноль делить нельзя")
+            {
+                InputBox.FontSize = 30;
+                InputBox.Text = "0";
+                ButtonOperationGrid.IsEnabled = true;
+                ButtonChangeSign.IsEnabled = true;
+                return;
+            }
+
+            if (ResultText.Content.ToString() == "" || ResultText.Content.ToString().IndexOf("=") != -1)
+            {
+                return;
+            }
+
+            Calculator.num_2 = InputBox.Text;
+
+            if(Convert.ToDouble(Calculator.num_2) < 0)
+            {
+                ResultText.Content += "(" + Calculator.num_2 + ")" + "=";
+            }
+            else
+            {
+                ResultText.Content += Calculator.num_2 + "=";
+            }
+
+            InputBox.Text = Calculator.CalculationResult();
+            Calculator.num_1 = InputBox.Text;
+            Calculator.num_2 = "";
+            Calculator.operation = "";
+            
+            if (InputBox.Text == "∞")
+            {
+                InputBox.FontSize = 23;
+                InputBox.Text = "На ноль делить нельзя";
+                Calculator.num_1 = "";
+                ButtonOperationGrid.IsEnabled = false;
+                ButtonChangeSign.IsEnabled = false;
+            } 
+        }
+        private void ButtonRange_Click(object sender, RoutedEventArgs e)
+        {
+            if (!ButtonRangeOnClick)
+            {
+                CalculatorWindow.Height = 500;
+                ButtonGrid.IsEnabled = false;
+                ButtonRangeOnClick = true;
+                return;
+            }
+
+            if (!InputRange())
+            {
+                return;
+            }
+
+            ButtonGrid.IsEnabled = true;
+            CalculatorWindow.Height = 395;
+            ButtonRangeOnClick = false;
+        }
+        private void TextBoxLimit_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+           if(!Char.IsDigit(e.Text, 0) && e.Text != "-")
+           {
+               e.Handled = true;
+           }
+        }
+        private void ButtonGrid_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (!ButtonGrid.IsEnabled)
+            {
+                ButtonGrid.Opacity = 0.5;
+            }
+            else
+            {
+                ButtonGrid.Opacity = 1;
+            }
+        }
+        private void ButtonOperationGrid_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (!ButtonOperationGrid.IsEnabled)
+            {
+                ButtonOperationGrid.Opacity = 0.5;
+                ButtonChangeSign.Opacity = 0.5;
+            }
+            else
+            {
+                ButtonOperationGrid.Opacity = 1;
+                ButtonChangeSign.Opacity = 1;
+            }
+        }
+        private void Button_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Button Num = (Button)sender;
+
+            DoubleAnimation buttonAnimationStart = new DoubleAnimation();
+            buttonAnimationStart.From = Num.FontSize;
+            buttonAnimationStart.To = 16;
+            buttonAnimationStart.Duration = TimeSpan.FromSeconds(0.1);
+            Num.BeginAnimation(Button.FontSizeProperty, buttonAnimationStart);
+        }
+        private void Button_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Button Num = (Button)sender;
+
+            DoubleAnimation buttonAnimationEnd = new DoubleAnimation();
+            buttonAnimationEnd.From = 16;
+            buttonAnimationEnd.To = 20;
+            buttonAnimationEnd.Duration = TimeSpan.FromSeconds(0.1);
+            Num.BeginAnimation(Button.FontSizeProperty, buttonAnimationEnd);
+        }
+        public void InputOperation(string Operation)
+        {
+            ResultText.Content = Calculator.num_1;
+            ResultText.Content += Operation;
+            Calculator.operation = Operation;
+            InputBox.Text = "0";
+        }
+        public bool InputRange()
+        {
+            if(TextBoxFirstLimit.Text.LastIndexOf("-") != 0 && TextBoxFirstLimit.Text.LastIndexOf("-") != -1)
+            {
+                MessageBox.Show("Первая граница не является числом",
+                                "Ошибка ввода",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Error
+                                );
+                return false;
+            }
+
+            if(TextBoxSecondLimit.Text.LastIndexOf("-") != 0 && TextBoxSecondLimit.Text.LastIndexOf("-") != -1)
+            {
+                MessageBox.Show("Вторая граница не является числом",
+                                "Ошибка ввода",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Error
+                                );
+                return false;
+            }
+            
+            if(Convert.ToDouble(TextBoxFirstLimit.Text) > Convert.ToDouble(TextBoxSecondLimit.Text))
+            {
+                MessageBox.Show("Первая граница не может быть больше второй",
+                "Ошибка ввода",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error
+                );
+                return false;
+            }
+
+            Calculator.firstLimit = Convert.ToDouble(TextBoxFirstLimit.Text);
+            Calculator.secondLimit = Convert.ToDouble(TextBoxSecondLimit.Text);
+
+            return true;
+        }
+    }
+}
